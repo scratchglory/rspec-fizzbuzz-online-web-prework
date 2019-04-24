cb7de1f916c871536cc125467f287391ca260c0c
Adella <adellakenn@gmail.com>
2019-04-24 18:38:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,7 +6,7 @@ def fizzbuzz(int)
     "Fizz" # Go fizz
   elsif int % 5 == 0 # if the number int is divisible by 5
     "Buzz" # Go buzz 
-  elsif ((int % 3) == 0 && (int % 5)) == 0 # if the number int is divisible by 3 and 5
+  elsif ((int % 3) == 0 && (int % 5) == 0) # if the number int is divisible by 3 and 5
     "FizzBuzz" #Go fizzbuzz
   else  # if the number int is NOT divisible by 3 or 5
     "nil" #Go nil 
