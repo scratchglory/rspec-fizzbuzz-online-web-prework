54a39c3a37ba6891ae4f8852fc01f07c611307ae
Adella <adellakenn@gmail.com>
2019-04-24 18:11:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-require_relative './spe'
+require_relative './spec_helper.rb'
+require_relative '../fizzbuzz.rb'
 
 def fizzbuzz
   
